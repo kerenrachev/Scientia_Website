93ec060e4a710653dfce00c85014f8da3921702c
Keren Rachev <61757368+kerenrachev@users.noreply.github.com>
2022-12-24 14:12:48 +0200

    first commit

---
+++ precache-manifest.34b063154fd1d0e2926c66f23aa19059.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "763db0a0e8ab60e985a0",
+    "url": "/Scientia_Website/static/css/main.fab21236.chunk.css"
+  },
+  {
+    "revision": "763db0a0e8ab60e985a0",
+    "url": "/Scientia_Website/static/js/main.763db0a0.chunk.js"
+  },
+  {
+    "revision": "7f1662d5cca94d6900a5",
+    "url": "/Scientia_Website/static/js/1.7f1662d5.chunk.js"
+  },
+  {
+    "revision": "c7416bf5214c3ac04182",
+    "url": "/Scientia_Website/static/css/2.e0317925.chunk.css"
+  },
+  {
+    "revision": "c7416bf5214c3ac04182",
+    "url": "/Scientia_Website/static/js/2.c7416bf5.chunk.js"
+  },
+  {
+    "revision": "571fefb284a0a917c7c3",
+    "url": "/Scientia_Website/static/js/runtime~main.571fefb2.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Scientia_Website/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "68913771b1046e22d895197973eaaec3",
+    "url": "/Scientia_Website/index.html"
+  }
+];
---
+++ unitybuild/UnityWebGL.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:259198b7dc4845c19d28036b2c0f431f5832c20909dd3f3732445de56dd1341e
+size 415583
